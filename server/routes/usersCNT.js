e80d9c6d317eeb77bc1b5eca88511d83bdcba4b7
Francesco Vicidomini <fr.vicidomini94@gmail.com>
2018-11-24 01:59:34 +0100

    added eslint

--- server/routes/usersCNT.js
+++ server/routes/usersCNT.js
@@ -6,27 +6,27 @@ const Op = singleton.Op;
 
 router.post('/login', function(req, res){
     user.findOne({where: {"username":req.body.username, "password":req.body.password} })
-    .then( doc => {if(doc===null)res.sendStatus(404).end({"message": "utente non trovato"});
-                   else res.status(200).send(doc)});
+        .then( doc => {if(doc===null)res.sendStatus(404).end({"message": "utente non trovato"});
+        else res.status(200).send(doc)});
 });
 
 
-router.post('/signin', function (req,res,err) {
-    user.create({"username": req.body.username,"password":req.body.password})
-    .then(doc=>res.send({"message": "registrato"}).status(200).end())
-    .catch(err=> res.sendStatus(409).end({"message": "già presente"}));
+router.post('/signin', function (req, res) {
+    user.create({"username": req.body.username, "password":req.body.password})
+        .then(doc=>res.send(doc).status(200).end())
+        .catch(err=>res.sendStatus(409).end(err));
 });
 
-router.get('/userlist',function(req,res,err){
-user.findAll({attributes: ['username']})//attributes sarebbe la SELECT
-.then(doc=>res.send(doc).status(200).end())
-.catch(err=> res.sendStatus(404).end({"message": "nessun utente"}));
+router.get('/userlist', function(req, res){
+    user.findAll({attributes: ['username']})//attributes sarebbe la SELECT
+        .then(doc=>res.send(doc).status(200).end())
+        .catch(err=> res.sendStatus(404).end(err));
 });
 
-router.delete('/deleteTest',function(req,res,err){
+router.delete('/deleteTest', function(req, res){
     user.destroy({where: {username: {[Op.like]: 'Test%'}}})
-    .then(doc=>res.send({"message": "eliminati"}).status(200).end())
-    .catch(err=> res.sendStatus(404).end({"message": "non ci sono tester"}));
+        .then(doc=>res.send(doc.body).status(200).end())
+        .catch(err=> res.sendStatus(404).end(err));
 });
 
 module.exports = router;
